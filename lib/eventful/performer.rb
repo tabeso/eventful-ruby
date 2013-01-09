d8b24202ed0f74c35cf7dcf04da69bf179d9af59
Gabriel Evans <gabriel@codeconcoction.com>
2013-01-09 07:18:55 -0800

    Add deserialization for performers

--- lib/eventful/performer.rb
+++ lib/eventful/performer.rb
@@ -16,5 +16,33 @@ module Eventful
       performer = instantiate(response.body['performer'])
       respond_with performer, response
     end
+
+    protected
+
+    def self.deserialize_attributes(attrs = {})
+      {
+        id: attrs[:id],
+        url: attrs[:url],
+        name: attrs[:name],
+        short_bio: attrs[:short_bio],
+        long_bio: attrs[:long_bio],
+        withdrawn: attrs[:withdrawn] == '1',
+        tags: attrs[:tags] ? attrs[:tags].collect { |t| t[:title] } : [],
+        images: deserialize_images(attrs),
+        links: attrs[:links] ? attrs[:links] : [],
+        created_at: attrs[:created],
+        updated_at: attrs[:modified]
+      }
+    end
+
+    private
+
+    def self.deserialize_images(attrs)
+      return [] unless attrs[:images] || attrs[:image]
+      attrs[:images] ||= [attrs[:image]]
+      attrs[:images].collect { |image| Image.instantiate(image) }.select do |image|
+        image.url.present?
+      end
+    end
   end
 end
